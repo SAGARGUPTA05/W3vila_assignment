653baf80789976b5dedd91b9e28cedf1cf1ea96c
Sagar Gupta <168528321+SAGARGUPTA05@users.noreply.github.com>
2025-05-29 17:46:18 +0530

    Update Login.jsx

--- frontend/client/src/pages/Login.jsx
+++ frontend/client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-const FRONTEND_URL = "http://localhost:5000";
+const FRONTEND_URL = "https://w3vila-assignment.onrender.com";
 const Login = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({ email: "", password: "" });
