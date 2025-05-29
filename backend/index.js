b0b81f1785804fd965119738e5b5f5c503ab8a4b
Sagar Gupta <168528321+SAGARGUPTA05@users.noreply.github.com>
2025-05-29 17:44:38 +0530

    Update index.js for render

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const db=require("./config/db")
 const authRouter=require("./routes/authRoute")
 const pollRouter=require("./routes/pollRoute")
+const path = require('path');
+const DIRNAME=path.resolve();
 
 db();
 const app = express();
@@ -15,8 +17,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
+
 app.use("/api/auth", authRouter);
 app.use("/api/polls", pollRouter );
-
-
+app.use(express.static(path.join(DIRNAME,"/frontend/client/dist")));
+app.use("*",(_,res)=>{
+  res.sendFile(path.resolve(DIRNAME,"frontend","client","dist","index.html"));
+})
 app.listen(process.env.PORT,()=>{ console.log("server running ",process.env.PORT)})
